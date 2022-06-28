39bf5ee544e8d514160be9ed30454631a8e691db
Hasan Iqtedar <hasan.iqtedar@gmail.com>
2022-06-28 20:14:38 +0500

    Update controllers for category detail

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,6 +1,7 @@
 var async = require('async');
 var { body, validationResult } = require('express-validator');
 var Category = require('../models/category');
+var Item = require('../models/item');
 
 exports.categoryList = function (req, res, next) {
   // res.send('Not Implemented: Category List');
@@ -18,7 +19,30 @@ exports.categoryList = function (req, res, next) {
 };
 
 exports.categoryDetail = function (req, res, next) {
-  res.send('Not Implemented: Category Detail: ' + req.params.id);
+  async.parallel(
+    {
+      category: function (callback) {
+        Category.findById(req.params.id).exec(callback);
+      },
+      category_items: function (callback) {
+        Item.find({ category: req.params.id }).sort({ name: 1 }).exec(callback);
+      },
+    },
+    function (err, results) {
+      if (err) {
+        return next(err);
+      }
+      if (results.category == null) {
+        var err = new Error('Category not found');
+        err.status = 404;
+        return next(err);
+      }
+      res.render('category_detail', {
+        category: results.category,
+        category_items: results.category_items,
+      });
+    }
+  );
 };
 
 exports.categoryCreateGet = function (req, res, next) {
