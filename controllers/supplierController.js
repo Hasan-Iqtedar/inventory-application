8f2fbd8801f4430f35ee1a07221f2848fb6f4e51
Hasan Iqtedar <hasan.iqtedar@gmail.com>
2022-06-29 15:15:19 +0500

    Update controllers to update a supplier

--- controllers/supplierController.js
+++ controllers/supplierController.js
@@ -91,12 +91,61 @@ exports.supplierCreatePost = [
 ];
 
 exports.supplierUpdateGet = function (req, res, next) {
-  res.send('Not Implemented: supplier Update Post: ' + req.params.id);
+  Supplier.findById(req.params.id).exec(function (err, result) {
+    if (err) {
+      return next(err);
+    }
+    res.render('supplier_form', { title: 'Update Supplier', supplier: result });
+  });
 };
 
-exports.supplierUpdatePost = function (req, res, next) {
-  res.send('Not Implemented: supplier Update Post: ' + req.params.id);
-};
+exports.supplierUpdatePost = [
+  body('name', 'Please provide name').trim().isLength({ min: 1 }).escape(),
+  body('description', 'Please provide description')
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+
+  (req, res, next) => {
+    const errors = validationResult(req);
+
+    var supplier = new Supplier({
+      _id: req.params.id,
+      name: req.body.name,
+      description: req.body.description,
+    });
+
+    if (!errors.isEmpty()) {
+      res.render('supplier_form', {
+        title: 'Update Supplier',
+        supplier: supplier,
+        errors: errors.array(),
+      });
+      return;
+    } else {
+      Supplier.findOne({ name: supplier.name }).exec(function (err, result) {
+        if (err) {
+          return next(err);
+        }
+        if (result) {
+          res.redirect(result.url);
+        } else {
+          Supplier.findByIdAndUpdate(
+            req.params.id,
+            supplier,
+            {},
+            function (err, updatedSupplier) {
+              if (err) {
+                return next(err);
+              }
+              res.redirect(updatedSupplier.url);
+            }
+          );
+        }
+      });
+    }
+  },
+];
 
 exports.supplierDeleteGet = function (req, res, next) {
   res.send('Not Implemented: supplier Delete Post: ' + req.params.id);
