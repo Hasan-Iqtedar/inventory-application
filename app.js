b3b1280806c935822477b04fa6b0dc6f0de01e4a
Hasan Iqtedar <hasan.iqtedar@gmail.com>
2022-06-29 15:36:53 +0500

    Ready for production

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 require('dotenv').config();
+var compression = require('compression');
 var createError = require('http-errors');
 var express = require('express');
+var helmet = require('helmet');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
@@ -11,6 +13,8 @@ var indexRouter = require('./routes/index');
 var groceriesRouter = require('./routes/groceries');
 
 var app = express();
+
+app.use(helmet());
 
 var mongoDB = process.env.MONGODB_URL;
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
@@ -26,6 +30,8 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(compression());
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
